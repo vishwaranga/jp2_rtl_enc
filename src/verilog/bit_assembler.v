88fe2bebf96131f73aa685367b6a72e3c92679d2
sajith <sajithvishwaranga@gmail.com>
2017-01-03 13:41:54 +0000

    state machinw

--- src/verilog/bit_assembler.v
+++ src/verilog/bit_assembler.v
@@ -34,7 +34,7 @@ module bit_assembler
 //---------------------------------------------------------------------------------------------------------------------
 // parameter definitions
 //---------------------------------------------------------------------------------------------------------------------
-    parameter               DATA_W      =   32,
+    parameter               DATA_W      =   128,
     parameter               KEEP_W      =   DATA_W/8,    
 
 )
@@ -45,11 +45,19 @@ module bit_assembler
     input                                   clk,
     input                                   rst_n,
 
-    input                                   s_axis_rx_valid_i,
-    input                                   s_axis_rx_last_i,
-    input                   [DATA_W-1:0]    s_axis_rx_data_i,
-    input                   [KEEP_W-1:0]    s_axis_rx_keep_i,
-    input reg                               s_axis_rx_ready_o,
+    input                                   s_axis_data_rx_valid_i,
+    input                                   s_axis_data_rx_last_i,
+    input                   [DATA_W-1:0]    s_axis_data_rx_data_i,
+    input                   [KEEP_W-1:0]    s_axis_data_rx_keep_i,
+    input reg                               s_axis_data_rx_ready_o,
+
+    input                                   s_axis_hdr_rx_valid_i,
+    input                                   s_axis_hdr_rx_last_i,
+    input                   [DATA_W-1:0]    s_axis_hdr_rx_data_i,
+    input                   [KEEP_W-1:0]    s_axis_hdr_rx_keep_i,
+    input reg                               s_axis_hdr_rx_ready_o,
+
+
 
     output reg                              m_axis_tx_valid_o,
     output reg                              m_axis_tx_last_o,
@@ -66,6 +74,29 @@ module bit_assembler
 // localparam definitions
 //---------------------------------------------------------------------------------------------------------------------
     localparam          NO_OF_BOX_STATES        = 7;
+
+    localparam          BIT_32                  = 32;
+    localparam          BIT_16                  = 16;
+    localparam          BIT_8                   = 8;
+    localparam          U_B_WIDTH               = 11;
+    localparam          E_B_WIDTH               = 5; 
+
+    localparam          IMG_WIDTH               = 32'd128;
+    localparam          IMG_HEIGHT              = 32'd128;
+    localparam          TILE_WIDTH              = 32'd128;
+    localparam          TILE_HEIGHT             = 32'd128;
+    localparam          NO_OF_CMP               = 16'd3;
+    localparam          ENUMES                    = 32'h10;               // 0x11 mono chrome // 0x10 RGB
+
+    localparam          LL_E                    = 11'd10;
+    localparam          HL_E                    = 11'd10;
+    localparam          LH_E                    = 11'd10;
+    localparam          HH_E                    = 11'd10;
+
+    localparam          LL_M                    = 5'd0;
+    localparam          HL_M                    = 5'd0;
+    localparam          LH_M                    = 5'd0;
+    localparam          HH_M                    = 5'd0;   
 
     localparam          JP2_SIGNATURE_L_BOX     = 32'h0000000C;
     localparam          JP2_SIGNATURE_T_BOX     = 32'h6A502020;
@@ -132,39 +163,172 @@ module bit_assembler
 //---------------------------------------------------------------------------------------------------------------------
 // Internal wires and registers
 //---------------------------------------------------------------------------------------------------------------------
+    reg             [NO_OF_BOX_STATES-1:0]      state_box;
+    reg             [TILE_CNT_W-1:0]			tile_index;
 
+    wire             [BIT_32-1:0]                width;
+    wire             [BIT_32-1:0]                height;
+    wire             [BIT_32-1:0]                t_width;
+    wire             [BIT_32-1:0]                t_height;
+    wire             [BIT_16-1:0]                no_of_cmp;
+    wire             [BIT_32-1:0]                enumes;
+
+    wire             [U_B_WIDTH-1:0]             llm,hlm,lhm,hhm;
+    wire             [E_B_WIDTH-1:0]             lle,hle,lhe,hhe;
 //---------------------------------------------------------------------------------------------------------------------
 // Implementation
 //---------------------------------------------------------------------------------------------------------------------
-    reg             [NO_OF_BOX_STATES-1:0]      state_box;
+
+assign width        = WIDTH;
+assign height       = HEIGHT;
+assign no_of_cmp    = NO_OF_CMP;
+assign meth         = ENUMES;
+assign t_width      = TILE_WIDTH;
+assign t_height     = TILE_HEIGHT;
+
+assign lle          = LL_E;
+assign hle          = HL_E;
+assign lhe          = LH_E;
+assign hhe          = HH_E;
+
+assign llm          = LL_M;
+assign hlm          = HL_M;
+assign lhm          = LH_M;
+assign hhm          = HH_M;
+
 always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
-        state_box                                   <= STATE_BOX_INIT;
-
-        s_axis_rx_ready_o                           <= 1'b0;
-
-        m_axis_tx_valid_o                           <= 1'b0;
-        m_axis_tx_last_o                            <= 1'b0;
-        m_axis_tx_data_o                            <= {DATA_W{1'b0}};
-        m_axis_tx_keep_o                            <= {KEEP_W{1'b0}};
+        state_box                                               <= STATE_BOX_INIT;
+            
+        s_axis_rx_ready_o                                       <= 1'b0;
+            
+        m_axis_tx_valid_o                                       <= 1'b0;
+        m_axis_tx_last_o                                        <= 1'b0;
+        m_axis_tx_data_o                                        <= {DATA_W{1'b0}};
+        m_axis_tx_keep_o                                        <= {KEEP_W{1'b0}};
     end 
     else begin
         case (state_box)    : begin
+            STATE_BOX_INIT  : begin
+                if(start) begin
+                    state_box                                   <= STATE_BOX_1;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_last_o                            <= 1'b0;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {32'h00000014,32'h0D0A870A,32'h6A502020,32'h0000000C};   // JP2_SIGNATURE_L_BOX //T_BOX//D_BOX// PROFILE_L_BOX     
+                end
+                else begin
+                    s_axis_rx_ready_o                           <= 1'b0;
+
+                    m_axis_tx_valid_o                           <= 1'b0;
+                    m_axis_tx_last_o                            <= 1'b0;
+                    m_axis_tx_data_o                            <= {DATA_W{1'b0}};
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b0}};  
+                end 
+            end
             STATE_BOX_1 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_2;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {32'h6A703220,32'h00000000,32'h6A703220,32'h66747970}; 
+                end
             end
             STATE_BOX_2 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_3;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {32'h69686472,32'h00000016,32'h6A703268,32'h0000002d}; 
+                end
             end
             STATE_BOX_3 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_4;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {16'h000f,8'h00,8'h00,8'h07,8'h07,no_of_cmp,height,width}; 
+                end
             end
             STATE_BOX_4 :   begin
-            end
-            STATE_BOX_4 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_5;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {2'b00,(KEEP_W-2){1'b1}};                                 // one byte is free
+                    m_axis_tx_data_o                            <= {8'h00,enumes,8'h00,8'h00,8'h01,32'h636F6C72,16'h0000};  
+                end
             end
             STATE_BOX_5 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_6;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {height,width,16'h0000,16'h002f,16'hff51,16'hff4f}; 
+                end
             end
             STATE_BOX_6 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_7;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {t_height,t_width,32'h0,32'h0}; 
+                end
             end
-        
+            STATE_BOX_7 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_8;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {8'h01,8'h01,8'h07,8'h01,8'h01,8'h07,no_of_cmp,32'h0,32'h0};
+                end
+            end
+            STATE_BOX_8 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_9;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {KEEP_W{1'b1}};
+                    m_axis_tx_data_o                            <= {8'h00,8'h04,8'h04,8'h01,8'h00,16'h0001,8'h02,8'h06,16'h000c,16'hff52,8'h01,8'h01,8'h07};  // op = 2  //delta_t = 1 // MC = 0
+                end
+            end
+            STATE_BOX_9 :   begin
+                if(m_axis_tx_ready_i) begin
+                    state_box                                   <= STATE_BOX_SOT;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {2'b00,(KEEP_W-2){1'b1}};
+                    m_axis_tx_data_o                            <= {hhe,hhm,lhe,lhm,hle,hlm,lle,llm,8'h42,16'h000B,16'hff5c,8'h00};         // WT = 0  // (Q == 2  G =2 (3+2*N_b))
+                end      
+            end
+            STATE_BOX_SOT:  begin
+                if(m_axis_tx_ready_i && length ready) begin                            //    check
+                    state_box                                   <= STATE_BOX_PKT_HDR;
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= {2'b00,(KEEP_W-2){1'b1}};
+                    m_axis_tx_data_o                            <= {16'hFF93,8'h02,tile_part_index,tile_part_length,tile_index,16'h000a,16'hFF90};    
+                end
+            end
+            STATE_BOX_PKT_HDR:  begin
+                if(m_axis_tx_ready_i && length ready && s_axis_hdr_rx_valid_i) begin  
+
+                    if(s_axis_hdr_rx_last_i) begin
+                    	state_box                               <= STATE_BOX_PKT_DATA;
+                    end
+
+                    m_axis_tx_valid_o                           <= 1'b1;
+                    m_axis_tx_keep_o                            <= s_axis_hdr_rx_keep_i;
+                    m_axis_tx_data_o                            <= s_axis_hdr_rx_data_i;    
+                end
+            end            
             default :   begin
             end
         endcase
